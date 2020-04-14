e0a175970f61e5c4044e4d7553909828095fa933
adalek <61150101+adalek@users.noreply.github.com>
2020-04-14 23:07:18 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,25 @@
-var wall1;
-var wall2;
-var wall3;
-var wall4;
-var wall5;
-var wall6;
-var wall7;
-var wall8;
-var wall9;
-var wall10;
-var wall11;
-var wall12;
-var wall13;
-var wall14;
-var wall15;
-var wall16;
+var positions = [
+  { x: 100, y: 650, width: 140, height: 70 }, // bottom right1
+  { x: 390, y: 650, width: 300, height: 70 }, // bottom right2
+  { x: 645, y: 650, width: 70, height: 70 },  // bottom left
+  { x: 135, y: 510, width: 70, height: 70 },  //line3, right1;
+  { x: 390, y: 510, width: 300, height: 70 }, //line3, right2;
+  { x: 200, y: 370, width: 400, height: 70 }, //line5 right;
+  { x: 70, y: 90, width: 140, height: 70 }, //top right1
+  { x: 235, y: 230, width: 330, height: 70 }, //line7 right;
+  { x: 340, y: 90, width: 260, height: 70 }, //top right2;
+  { x: 540, y: 125, width: 140, height: 140 }, //top left;
+  { x: 575, y: 230, width: 70, height: 70 }, //line7 left;
+  { x: 595, y: 370, width: 240, height: 70 }, //line5 left;
+];
+
+var walls;
+
+var button1;
+var water;
+var fire;
 var spr2;
-var spr4;
+var box;
 var spr;
 var img;
 function setup() {
@@ -23,58 +27,15 @@ function setup() {
   createCanvas(700, 700);
   background(255, 250, 240);
   img = loadImage('./gameover.jpg')
-  //bottom right1;
-  wall1 = createSprite(
-    100, 650, 140, 70);
-  wall1.shapeColor = color(0);
-  //bottom right2
-  wall2 = createSprite(
-    390, 650, 300, 70);
-  wall2.shapeColor = color(0);
-  //bottom left
-  wall3 = createSprite(
-    645, 650, 70, 70);
-  wall3.shapeColor = color(0);
-  //count from bottom, line2, right;
-  box = createSprite(
-    205, 580, 70, 70);
-  box.shapeColor = color(139, 69, 19);
-  //line3 right1
-  wall5 = createSprite(
-    135, 510, 70, 70);
-  wall5.shapeColor = color(0);
-  //line3 right2
-  wall6 = createSprite(
-    390, 510, 300, 70);
-  wall6.shapeColor = color(0);
-  //line5 right
-  wall7 = createSprite(
-    200, 370, 400, 70);
-  wall7.shapeColor = color(0);
-  //top right1
-  wall8 = createSprite(
-    70, 90, 140, 70);
-  wall8.shapeColor = color(0);
-  //line7 right;
-  wall9 = createSprite(
-    235, 230, 330, 70);
-  wall9.shapeColor = color(0);
-  //top right2;
-  wall10 = createSprite(
-    340, 90, 260, 70);
-  wall10.shapeColor = color(0);
-  //top left;
-  wall11 = createSprite(
-    540, 125, 140, 140);
-  wall11.shapeColor = color(0);
-  //line7 left
-  wall12 = createSprite(
-    575, 230, 70, 70);
-  wall12.shapeColor = color(0);
-  //line5 left
-  wall13 = createSprite(
-    595, 370, 240, 70);
-  wall13.shapeColor = color(0);
+
+  function createWall({ x, y, width, height }) {
+    let wall = createSprite(x, y, width, height);
+    wall.shapeColor = color(0);
+    return wall;
+  }
+  walls = positions.map(createWall);
+  (645, 650, 70, 70);
+
   //line4 left;
   fire = createSprite(
     510, 440, 70, 70);
@@ -83,6 +44,10 @@ function setup() {
   water = createSprite(
     235, 300, 70, 70);
   water.shapeColor = color(0, 0, 100);
+  //count from bottom, line2, right;
+  box = createSprite(
+    205, 580, 70, 70);
+  box.shapeColor = color(139, 69, 19);
   //button
   button1 = createSprite(235, 180, 20, 20);
 
@@ -102,7 +67,7 @@ function draw() {
 
 
 
-  walls = [wall1, wall2, wall3, wall5, wall6, wall7, wall8, wall9, wall10, wall11, wall12, wall13];
+  // walls = [wall1, wall2, wall3, wall5, wall6, wall7, wall8, wall9, wall10, wall11, wall12, wall13];
   for (i = 0; i < 12; i++) {
     spr2.collide(walls[i]);
     spr.collide(walls[i]);
